b480d32c25ec208d4c504798089c49c7ac300455
Violet <spynetgirl@outlook.com>
2024-02-12 22:37:34 +0200

    improved verbose messages

--- WinSecureDNSMgr/Module/MainExt/PSDefaultParameterValues.ps1
+++ WinSecureDNSMgr/Module/MainExt/PSDefaultParameterValues.ps1
@@ -2,11 +2,17 @@
 # This is why this file is dot-sourced in every other component of the WinSecureDNSMgr module at the beginning
 $PSDefaultParameterValues = @{
     'Invoke-WebRequest:HttpVersion'                     = '3.0'
-    'Invoke-WebRequest:SslProtocol'                     = 'Tls12,Tls13'
+    'Invoke-WebRequest:SslProtocol'                     = 'Tls13'
     'Invoke-RestMethod:HttpVersion'                     = '3.0'
-    'Invoke-RestMethod:SslProtocol'                     = 'Tls12,Tls13'
+    'Invoke-RestMethod:SslProtocol'                     = 'Tls13'
+    'Invoke-RestMethod:Verbose'                         = $false
+    'Invoke-WebRequest:Verbose'                         = $false
     'Import-Module:Verbose'                             = $false
     'Export-ModuleMember:Verbose'                       = $false
+    'Set-ScheduledTask:Verbose'                         = $false
+    'Register-ScheduledTask:Verbose'                    = $false
+    'New-CimInstance:Verbose'                           = $false
+    'Get-CimClass:Verbose'                              = $false
     'Get-ActiveNetworkAdapterWinSecureDNS:Verbose'      = $Verbose
     'Get-ManualNetworkAdapterWinSecureDNS:Verbose'      = $Verbose
     'Get-IPv4DoHServerIPAddressWinSecureDNSMgr:Verbose' = $Verbose
